6f82781d28a4bede6e1d4c33649e6ecf1feeae26
xkan-32 <121345057+xkan-32@users.noreply.github.com>
2023-04-18 13:08:32 +0900

    fmt実施

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -89,11 +89,11 @@ module "my_RDS" {
   NameBase = var.NAMEBASE
   env      = var.ENV
 
-  InstanceClass           = "db.t2.small"
-  MysqlVession            = "8.0.28"
-  Username                = "admin"
+  InstanceClass = "db.t2.small"
+  MysqlVession  = "8.0.28"
+  Username      = "admin"
   # CircleCIの環境変数使用
-  rdspassword = var.CIRCLECI_RDS_PASSWORD
+  rdspassword             = var.CIRCLECI_RDS_PASSWORD
   Storage                 = "20"
   database_name           = "water"
   storage_type            = "gp2"
