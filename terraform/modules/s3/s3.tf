03937631ad5986ff5d119a93fd20b5693b5ede98
xkan-32 <121345057+xkan-32@users.noreply.github.com>
2023-04-19 21:24:18 +0900

    ACL設定変更

--- terraform/modules/s3/s3.tf
+++ terraform/modules/s3/s3.tf
@@ -5,10 +5,10 @@ resource "aws_s3_bucket" "contents_bucket" {
   bucket = "teradacontentsbucket-${var.NameBase}"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.contents_bucket.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "example" {
+#   bucket = aws_s3_bucket.contents_bucket.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_versioning" "contents_bucket" {
   bucket = aws_s3_bucket.contents_bucket.id
