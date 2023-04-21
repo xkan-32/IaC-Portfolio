8dd4b77706c1cb9d571ba17750ddcd46f503d709
xkan-32 <121345057+xkan-32@users.noreply.github.com>
2023-04-21 17:08:55 +0900

    アウトバンドルール追加

--- terraform/modules/securitygroup/securitygroup.tf
+++ terraform/modules/securitygroup/securitygroup.tf
@@ -33,6 +33,14 @@ resource "aws_security_group" "TeradaSecurityGroup01" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
     Environment = var.env
   }
@@ -58,6 +66,15 @@ resource "aws_security_group" "TeradaSecurityGroup02" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
   tags = {
     Environment = var.env
   }
@@ -76,6 +93,15 @@ resource "aws_security_group" "TeradaSecurityGroup03" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Environment = var.env
   }
